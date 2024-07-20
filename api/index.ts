ad47d7cbdac1c3c9c6d2fe038324315b7cd30e86
Jose-henry <joseph.henry@stu.cu.edu.ng>
2024-07-20 22:43:23 +0100

    messages

--- api/index.ts
+++
@@ -1,5 +0,0 @@
-import app from '../src/server'
-import apps from '../src/server.default'
-
-export default app
-export { apps }
---
+++ src/app/api/route.ts
@@ -0,0 +1,5 @@
+import app from '../../server'
+import apps from '../../server.default'
+
+export default app
+export { apps }
